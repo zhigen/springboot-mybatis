a477aefd7adbf9d989b8dd108b02a37ad98907c0
zglu <385861131@qq.com>
2020-03-06 16:53:50 +0800

    优化Test

--- src/test/java/com/zglu/mybatis/ApplicationTests.java
+++ src/test/java/com/zglu/mybatis/ApplicationTests.java
@@ -2,12 +2,15 @@ package com.zglu.mybatis;
 
 import com.zglu.mybatis.mapper.UserMapper;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
 
 /**
  * @author zglu
@@ -24,7 +27,9 @@ class ApplicationTests {
     @Test
     void contextLoads() {
         int id = 1;
-        log.info(userMapper.findById(id));
+        Map map = userMapper.findById(id);
+        Assertions.assertNotNull(map, "test");
+        log.info(map);
     }
 
 }
